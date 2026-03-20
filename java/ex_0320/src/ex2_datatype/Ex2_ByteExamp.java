2054ed98f44105bbb291de734908b7e3165b7d58
harln1 <baghalin199@gmail.com>
2026-03-20 18:27:08 +0900

    Create Ex2_ByteExamp.java

---
+++ java/ex_0320/src/ex2_datatype/Ex2_ByteExamp.java
@@ -0,0 +1,18 @@
+package ex2_datatype;
+
+public class Ex2_ByteExamp {
+
+	public static void main(String[] args) {
+		// 정수의 경우 해당 자료형이 표현할 수 있는
+		//범위를 벗어난 데이터를 저장하면
+		//오버플로우, 언더플로우가 발생한다
+		byte var1 = -129;//언더 플로우
+		System.out.println("var1:"+var1);
+		
+		byte var2 = 127;
+		byte var3 = 128;//오버 플로우
+		
+
+	}
+
+}
