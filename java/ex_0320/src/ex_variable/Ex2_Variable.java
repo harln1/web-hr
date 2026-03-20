dbfa13ea3f1a7ec4b8cc0ed6a5ac813fecc00c5a
harln1 <baghalin199@gmail.com>
2026-03-20 18:26:55 +0900

    Create Ex2_Variable.java

---
+++ java/ex_0320/src/ex_variable/Ex2_Variable.java
@@ -0,0 +1,21 @@
+package ex_variable;
+
+public class Ex2_Variable {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		int a  ;
+		 
+		//변수 x에 들어있는 값과 y에 들어있는 값을 교환하기
+		a = x;
+		x = y; 
+		y = a;
+		System.out.println("x="+x);
+		
+		
+		
+	
+		
+	}
+
+}
