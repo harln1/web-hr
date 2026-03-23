e645d457b6ede3dfb7bdb3931716df59a069847f
harln1 <baghalin199@gmail.com>
2026-03-23 13:47:17 +0900

    Create CharExample.java

---
+++ java/ex_0323/src/ex1_datatype/CharExample.java
@@ -0,0 +1,24 @@
+package ex1_datatype;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		//문자형 자료형 : char
+		//문자형 데이터는 무조건 한글자
+		//데이터를 ' ' 에 담아야한다
+		char c1 = 'A'; 
+		char c2 = '가';
+		char c3 = 65;//아스키코드
+		char c4 = 44032;//유니코드
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+		
+		
+
+	}
+
+}
