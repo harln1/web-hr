431e3dbd4c053c0956170efee1328cc4639d9901
harln1 <baghalin199@gmail.com>
2026-03-23 15:05:08 +0900

    Update PromotionExample.java

--- java/ex_0323/src/ex2_casting/PromotionExample.java
+++ java/ex_0323/src/ex2_casting/PromotionExample.java
@@ -4,6 +4,9 @@ public class PromotionExample {
 
 	public static void main(String[] args) {
 		//자동 타입 변환
+		//1.작은자료형 -> 큰자료형
+		//2.문자형데이터 -> 정수
+		
 		//작은 자료형에서 -> 큰 자료형으로 바꾸려 할때 자동으로 변환이 된다.
 		byte byteValue = 10;
 		int intValue = byteValue;
