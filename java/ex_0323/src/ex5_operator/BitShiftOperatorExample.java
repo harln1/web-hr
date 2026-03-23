f23baae8e8f3967dd71695d4ba157ad064e18130
harln1 <baghalin199@gmail.com>
2026-03-23 18:06:25 +0900

    Create BitShiftOperatorExample.java

---
+++ java/ex_0323/src/ex5_operator/BitShiftOperatorExample.java
@@ -0,0 +1,11 @@
+package ex5_operator;
+
+public class BitShiftOperatorExample {
+	public static void main(String[] args) {
+		int num1 = 1;
+		int result1 = num1 << 3;
+		int result2 = num1 * (int)Math.pow(2, 3);
+		System.out.println(result1);		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+	}
+
+}
