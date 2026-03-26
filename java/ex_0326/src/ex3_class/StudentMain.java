797444bf101a877acd5adb67a513717bd82231be
harln1 <baghalin199@gmail.com>
2026-03-26 18:29:17 +0900

    Update StudentMain.java

--- java/ex_0326/src/ex3_class/StudentMain.java
+++ java/ex_0326/src/ex3_class/StudentMain.java
@@ -21,6 +21,14 @@ public class StudentMain {
 		System.out.println("이름: "+a.name);
 		System.out.println("나이: "+a.age);
 		System.out.println("점수: "+a.score);
-	}
-
+		
+		 
+	        for (int i = 0; i < stdArray.length; i++) {
+	            System.out.println((i + 1) + ". 이름: " + stdArray[i].name + 
+	                               ", 나이: " + stdArray[i].age + 
+	                               ", 점수: " + stdArray[i].score);
+	        }
+        
+     
+ }
 }
