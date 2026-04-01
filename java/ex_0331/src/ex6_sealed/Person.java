ea71b203c176c05429f0064a025173b63173ffee
harln1 <baghalin199@gmail.com>
2026-04-01 09:22:12 +0900

    Update Person.java

--- java/ex_0331/src/ex6_sealed/Person.java
+++ java/ex_0331/src/ex6_sealed/Person.java
@@ -3,6 +3,7 @@ package ex6_sealed;
 // 1. sealed 클래스 선언: Employee와 Manager 클래스에게만 상속을 허용함
 public sealed class Person permits Employee, Manager {
     String name;
+    
 
     public void work() {
         System.out.println("하는 일이 결정되지 않았습니다");
