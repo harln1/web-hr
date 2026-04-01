723aa5eef7d4b280492c07d53159eab1213b81ae
harln1 <baghalin199@gmail.com>
2026-04-01 10:06:31 +0900

    2025-04-10

--- java/ex_0331/src/ex6_sealed/Person.java
+++ java/ex_0331/src/ex6_sealed/Person.java
@@ -8,4 +8,6 @@ public sealed class Person permits Employee, Manager {
     public void work() {
         System.out.println("하는 일이 결정되지 않았습니다");
     }
+    
+    
 }
---
+++ java/ex_0401/src/ex1_abstract/Tiger.java
@@ -0,0 +1,10 @@
+package ex1_abstract;
+
+public class Tiger extends Animal{
+	
+	@Override
+	public void sound() {
+		System.out.println("어흥");
+	}
+
+}
