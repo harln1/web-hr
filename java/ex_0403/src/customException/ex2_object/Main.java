9db4014e2e5f16bd3f4e2ff1602e6cb6488001d2
harln1 <baghalin199@gmail.com>
2026-04-03 18:20:37 +0900

    2025

--- java/ex_0403/src/customException/ex2_object/Main.java
+++ java/ex_0403/src/customException/ex2_object/Main.java
@@ -35,24 +35,24 @@ public class Main {
         //Employee 객체면 이름과 부서를 출력해주세요
 
         //instanceof 활용
-        
+
         for (Object obj: arr) {
-        
-            if (obj instanceof String)
-            	System.out.println("문자열데이터 : "+obj);
-            	
-            else if(obj instanceof Integer) 
-            	System.out.println("정수데이터: "+obj); 
-            
-            else if(obj instanceof Double) 
-            	System.out.println("실수데이터: "+ obj);
-            
-            else if(obj instanceof Employee ) {
-            	Employee e = (Employee)obj;
-            	System.out.println("사원명 : " + e.name);
-            	System.out.println("부서 : " + e.dept);
-            }
-            	
+
+        	if (obj instanceof String)
+        		System.out.println("문자열데이터 : "+obj);
+
+        	else if(obj instanceof Integer) 
+        		System.out.println("정수데이터: "+obj); 
+
+        	else if(obj instanceof Double) 
+        		System.out.println("실수데이터: "+ obj);
+
+        	else if(obj instanceof Employee ) {
+        		Employee e = (Employee)obj;
+        		System.out.println("사원명 : " + e.name);
+        		System.out.println("부서 : " + e.dept);
+        	}
+
         }
 	}
 }
