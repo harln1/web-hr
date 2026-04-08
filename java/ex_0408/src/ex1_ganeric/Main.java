790ec3c7f7c9f4236d7484cf3fbbb510a2a167da
harln1 <baghalin199@gmail.com>
2026-04-08 18:29:14 +0900

    tg

--- java/ex_0408/src/ex1_ganeric/Main.java
+++ java/ex_0408/src/ex1_ganeric/Main.java
@@ -58,7 +58,7 @@ public class Main {
     		total += n.doubleValue();
     	}
     	return total;
-    }
+    } 
     
     
     
---
+++ java/ex_0408/src/ex1_reflection/exam/SmartePhone.java
@@ -0,0 +1,23 @@
+package ex1_reflection.exam;
+
+public class SmartePhone {
+	String model;
+	int price;
+	
+	public SmartePhone (String model,int price) {
+		this.model = model;
+		this.price = price;		
+	}
+	
+	public void showInfo() {
+		System.out.println("모델명:"+model + "가격: "+price);
+	}
+	
+	public static void main(String[] args) {
+		SmartePhone smartephone = new SmartePhone("iPhone", 100);
+		
+		smartephone.showInfo();//showInfo() 메서드는 반환 타입이 void 그냥 sysout에 넣지 않고 이렇게 만 써서 출력
+		
+	}
+
+}
