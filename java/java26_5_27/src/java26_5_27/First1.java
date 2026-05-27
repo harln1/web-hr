988984c33e6142e1dc48f983ca2eb309bc8d29c0
harln1 <baghalin199@gmail.com>
2026-05-27 14:27:13 +0900

    fg

---
+++ java/java26_5_27/src/java26_5_27/First1.java
@@ -0,0 +1,18 @@
+package java26_5_27;
+
+public class First1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow World");
+		
+		int count; // 변수 선언 :타입(int..) + 변수이름 
+		
+		count = 10;
+		
+		count = 20;
+		
+		System.out.println(count);
+	}
+
+}
